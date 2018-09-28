ee75f4a73691ecdf06c3d31de90e329c88482375
javierhell <miromancequimico_004@hotmail.com>
2018-09-28 17:37:41 -0500

    este es mi primer commit

---
+++ src/prueba.js
@@ -0,0 +1,5 @@
+
+este es un archivo nuevo en la carpeta de nuestro repositorio,
+al hacer el git status deberia ser reconocido como un cambio ennuestro repositorio de git
+
+ahora hay dos cambios el archivo y la carpeta vacia
