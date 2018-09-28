b69c41f87d81ae41128ee0ee735a094d83224351
“rychdaniels” <rychraya@gmail.com>
2018-09-28 18:48:33 -0500

    con el rych

--- src/fili.js
+++ src/fili.js
@@ -1,1 +1,9 @@
 Este archivo solo debe verse en mi rama
+
+en este caso se puede ver que la rama fili ya tiene el comit nuevo, pero el master deberia 
+continuar con el mismo contenido ya que no se ha trabajado en el,
+me cambiare a la rama del master que tengo de forma localStorage
+
+como puedes ver ya no se encuentra el archivo fili.js y tampoco se ve el commit en el log
+
+si me regreso a la rama de fili, estos reaparecen
